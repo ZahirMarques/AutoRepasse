c4993e10f07ace8663fb7b2f29b60a266d084ea5
Eduardo Borges <eduardoborgesbn@gmail.com>
2024-09-26 14:12:48 -0300

    aplicado o tailwind na tela de login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,20 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Document</title>
 </head>
 <body>
-    <h1>Olá</h1>
-    <img src="foto.png" alt="logo">
-    <br>
-    
-    <label for="login">
-        <a href="{{url('/login')}}">Login</a>
-    </label>
-    <br>
+    <div class="bg-cover grid h-screen object-center bg-gradient-to-l from-white from-65% to-indigo-500 place-items-center min-h-screen">
+        <div class="grid grid-cols-2 gap-4 h-5/6 w-11/12"> 
+            
+            <div class="grid grid-rows-3 grid-flow-col place-items-center">
+                <div class="w-9/12 h-5/6 pt-14"> <img src="{{ asset('img/autorepasse.png') }}" alt="Imagem AutoRepasse"> </div>
+                <div class="w-9/12 h-4/6 text-2xl font-semibold leading-snug text-justify text-indigo-500"><p>Cadastre-se agora na AutoRepasse e transforme sua concessionária com a melhor solução em gestão de vendas automotivas, garantindo eficiência, produtividade e agilidade na busca de informações.</p></div>
+                <div class="w-9/12 h-4/6 mb-8"><button class="shadow-lg shadow-gray-600 px-4 py-2 bg-white text-indigo-500 border-2 font-bold border-indigo-500 rounded-lg hover:bg-indigo-700 hover:text-white focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50" onclick="window.location.href='/register'">Cadastre-se</button></div>
+            </div>
+        
+            <div class="border-l-2 border-gray-400 grid-flow-col place-items-center">
+                <div class="mt-10 text-center text-4xl font-bold leading-9 tracking-tight text-gray-900 pt-12"><h1>Já possui uma conta?</h1></div>
+                <div class="sm:mx-auto sm:w-full sm:max-w-sm border-2 border-indigo-500 rounded-lg mt-12 px-7 py-7 row-span-3 mb-30 shadow-2xl shadow-gray-400">
 
-    <label for="register">
-        <a href="{{url('/register')}}">Register</a>
-    </label>
+                    <form action="{{url('/login')}}" method="POST">            
+                    @csrf
+
+                    <div class="">
+                    <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                    <input type="text" name="email" required class="mt-1 block w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Digite seu email"><br>
+                    </div>
+
+                    <div> 
+                    <label for="password" class="block text-sm font-medium text-gray-700">Senha</label>
+                    <input type="password" name="password" id="password" required class="mt-1 block w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Digite sua senha"><br>
+                    </div>
+
+                    <button class="w-full bg-indigo-500 text-white py-2 font-bold rounded hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"> Login</button>
+                    </form>
+
+                </div> 
+
+            </div>
+        </div>
+    </div>  
 </body>
 </html>
